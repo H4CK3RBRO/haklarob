b2e94edea30bc1d2ee3e6ff990b6293c33f97778
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-05-28 18:33:05 +0300

    Update duyur.js

--- komutlar/duyur.js
+++ komutlar/duyur.js
@@ -4,7 +4,7 @@ module.exports = {
     const kanal = message.mentions.channels.first();
     const args = message.content.split(' ').slice(2)
     const botmesajı = args.join(" ")
-    if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Duyuru yapmak için **Moderatör** olmanız lazım.');
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Duyuru yapmak için Yetkiniz yok.');
     if (!botmesajı) return message.reply('Duyuruyu yazmadınız.');
     if (!kanal) return message.reply('Kanalı Tanımlamadınız .');
     message.delete(message.author)
