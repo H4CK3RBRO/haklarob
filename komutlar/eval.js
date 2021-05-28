dbac448bc5dc7acbde73c3da8ea36a1a6f9dac06
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-05-28 19:30:58 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -13,7 +13,10 @@ module.exports = {
       .setField("Girdi", "```js\n" + args.join(" ") + "\n```")
       .setField("Çıktı", "```js\n" + komut + "\n```")
       .setColor('GREEN')
-      message.channel.send(embed)
+      message.channel.send(embed).then(mesaj => {
+      setTimeout(function () {
+        mesaj.delete()
+      }, 5000);
       message.react("✅")
     } else {
       let embed = new MessageEmbed()
@@ -21,7 +24,10 @@ module.exports = {
       .addField("Girdi", "```js\n" + args.join(" ") + "\n```")
       .addField("Çıktı", "```\n" + require('util').inspect(komut) + "\n```")
       .setColor('GREEN')
-      message.channel.send(embed)
+      message.channel.send(embed).then(mesaj => {
+      setTimeout(function () {
+        mesaj.delete()
+      }, 5000);
       message.react("✅")
     }
   } catch(err){
@@ -30,7 +36,11 @@ module.exports = {
     .addField("Girdi", "```js\n" + args.join(" ") + "\n```")
     .addField("Çıktı", "```js\n" + err + "\n```")
     .setColor('RED')
-    message.channel.send(embed)
+    message.channel.send(embed).then(mesaj => {
+      setTimeout(function () {
+        mesaj.delete()
+      }, 5000);
+    )}
     message.react("❌")
   }
   }
