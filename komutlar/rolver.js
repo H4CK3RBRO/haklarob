6b1133997d88cec3f65b015058605fd307946cfa
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-05-29 19:36:37 +0300

    Update rolver.js

--- komutlar/rolver.js
+++ komutlar/rolver.js
@@ -1,7 +1,6 @@
 module.exports = {
   kod: "rolver",
   async run (client, message, args){
-      if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Bunu yapamazsın')
       let role = message.mentions.roles.first();
       let member = message.mentions.members.first();
       member.roles.add(role);
