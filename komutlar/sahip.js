6ca507202cb6ef1adf8a995eb41dff3951efb6dd
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-05-30 12:52:40 +0300

    Update sahip.js

--- komutlar/sahip.js
+++ komutlar/sahip.js
@@ -1,8 +1,8 @@
 module.exports = {
   kod: "sahip",
   async run (client, message, args){
-    if (message.author.id !== "709349764113629236") {
-      message.author.send("Sahibim <@709349764113629236>")
+    if (message.author.id !== "709349764113629236,781105885878550528") {
+      message.author.send("Sahibim <@709349764113629236> ve <@781105885878550528> ")
     } else {
       message.channel.send("Sen benim sahibimsin")
     }
