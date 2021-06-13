62e7d737953f439460e5b9dff1bfc0b40ba7b43e
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-06-13 17:05:50 +0300

    Add files via upload

--- komutlar/hackle.js
+++ komutlar/hackle.js
@@ -7,11 +7,11 @@ module.exports = {
     if (isNaN(time)) return;
     const embed1 = new MessageEmbed()
     .setTitle('👨‍💻 Hackleme İşlemi Başladı.')
-    .setDescription(`${message.author}`, kişiargs )
+    .setDescription(`${message.author}`, kişi )
     .setImage('https://i.giphy.com/media/iFOVMvOHlCCKEQ8PBq/giphy.gif')
     const embed2 = new MessageEmbed()
     .setTitle('👨‍💻 HACKLEME BAŞARILI! 👨‍💻')
-    .setDescription(`${message.author}`, kişiargs , 'Adlı kullanıcıyı Hackledi')
+    .setDescription(`${message.author}`, kişi , 'Adlı kullanıcıyı Hackledi')
     .setImage('https://blog.internetvista.com/wp-content/uploads/2018/02/website-defacement.gif')
     message.channel.send(embed1).then(embed => {
     setTimeout(function () {
