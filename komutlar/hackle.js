1d75839591c693c597709cc66548cbc1b30dbc2b
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-06-22 17:37:29 +0300

    Update hackle.js

--- komutlar/hackle.js
+++ komutlar/hackle.js
@@ -6,11 +6,11 @@ module.exports = {
     let kişi = kişiargs.join(" ")
     const embed1 = new MessageEmbed()
     .setTitle('👨‍💻 Hackleme İşlemi Başladı...')
-    .setDescription(message.author + kişi + "'i Hackliyor...'" )
+    .setDescription( '<@' + message.author + '>' + kişi + "'i Hackliyor...'" )
     .setImage('https://i.giphy.com/media/iFOVMvOHlCCKEQ8PBq/giphy.gif')
     const embed2 = new MessageEmbed()
     .setTitle('👨‍💻 HACKLEME BAŞARILI! 👨‍💻')
-    .setDescription( message.author + kişi + 'Adlı kullanıcıyı Hackledi.')
+    .setDescription( '<@' + message.author + '>' + kişi + 'Adlı kullanıcıyı Hackledi.')
     .setImage('https://blog.internetvista.com/wp-content/uploads/2018/02/website-defacement.gif')
     message.channel.send(embed1).then(embed => {
     setTimeout(function () {
