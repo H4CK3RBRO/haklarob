26f1cfc3df309f495049cde27451a035b64200aa
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-06-27 17:49:23 +0300

    Update sil.js

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -1,12 +1,12 @@
 module.exports = {
   kod: "sil",
   async run (client, message, args){
-    if (isNaN(args)) return message.reply('Lütfen Bir Değer Giriniz');
+    if (isNaN(args)) return message.reply('🔢 Lütfen Bir Değer Giriniz 🔢');
     if (args < 2 || args > 100) return message.reply('Lütfen 2-100 Arasında Bir Değer Giriniz')
     message.channel.bulkDelete(Number(args))
     const { MessageEmbed} = require('discord.js')
     const embed = new MessageEmbed()
-    .setTitle('Mesajlar Başarıyla ```Silindi```.')
+    .setTitle('🗑 Mesajlar Başarıyla ```Silindi``` 🗑')
     .setDescription('Silinen Mesaj Sayısı: ' + args)
     .setColor("RED")
     message.channel.send(embed).then(mesaj => {
