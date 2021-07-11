9d902c507f5f37795747e111a5f67783de853fb6
H4CK3RBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2021-07-11 17:48:56 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,7 +6,6 @@ module.exports = {
     .setAuthor('HAKLAROB', 'https://cdn.discordapp.com/attachments/837587117906722826/842370025444737034/HAKLAROB.jpg')
     .setTitle('KOMUTLAR')
     .setColor("YELLOW")
-    .setThumbnail(../haklarob.png)
     .addField('?genel', 'Herkesin Kullanabileceği genel komutlar.')
     .addField('?eğlence', 'Bota eklenen yeni eğlence komutları')
     .addField('?moderatör', 'Moderatör komutları.')
