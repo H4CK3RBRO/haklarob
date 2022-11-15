c2e1ab2e296c845f359ba6a32a9faeab8b5f6b37
HACKERBRO <78161588+H4CK3RBRO@users.noreply.github.com>
2022-11-15 22:22:24 +0300

    Update haklarob.js

--- haklarob.js
+++ haklarob.js
@@ -1,13 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const işaret = require('./işaret.json');
-const hedefimiz = require('./hedef.json');
 const { readdirSync } = require('fs');
 const { join } = require('path');
 
 client.commands= new Discord.Collection();
-
-const prefix = "?"
 
 const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js"));
 
@@ -16,45 +12,20 @@ for (const file of commandFiles) {
     client.commands.set(command.kod, command);
 }
 
-client.on("error", console.error);
-
 client.on('ready', () => {
     console.log('Bot Aktif :) Created By: Yusuf Ahmet Eden')
     const durumlar = [
-      "Hergün Daha Da Gelişen Bot.",
-      "Yapımcı: HACKERBRO#7348",
-      "https://discord.ly/haklarob",
-      "Yeni Güncelleme",
-      "Prefix = ?",
-      "Gelişmeye Devam Edeceğiz :)",
-      "Hedef Sunucu: Sınır yok :)",
-      "?yardım",
+      ":)",
+      ":D",
+      ":)",
+      ":|",
+      ":(",
+      ":|"
     ]
     setInterval(function () {
       let durum = durumlar[Math.floor(Math.random()*durumlar.length)]
       client.user.setActivity(durum)
     }, 5000);
-});
-
-client.off('ready', () => {
-    console.log('Bot Aktif :) Created By: Yusuf Ahmet Eden')
-    client.user.setActivity('Şuan Büyük Bir Bakımda... Birazdan Tam hizmete Sunulacaktır.')
-});
-
-client.on("message", async message => {
-
-    if(message.author.bot) return;
-
-    if(message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-        if(!client.commands.has(command)) return message.channel.send(`Komut dosyamda **${command}** adlı bir komut bulamadım. Hemen Geliştiricime Bildir.`);
-        try {
-            client.commands.get(command).run(client, message, args);
-        } catch (error){
-            console.error(error);
-        }
-    }
 });
 
 client.on('message', message => {
@@ -121,4 +92,4 @@ client.on('message', message => {
   }
 });
 
-client.login('');
+client.login('NzA5MzQ5NzY0MTEzNjI5MjM2.GoFYdL.g5gNtopL9XGMFvHPruYNLrScMJdmEGuiS53rUI');
